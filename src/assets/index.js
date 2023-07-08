e30919efa9dea755c30ee1ec6c5f5084b4a69f0e
Jason Limas <jasonandrea14@gmail.com>
2023-07-09 06:23:29 +0700

    Import image assets

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import temulogoinvert from './temulogoinvert.png';
+import temuhome from './temuhome.png';
+import temuinterest from './temuinterest.png';
+import temugoals from './temugoals.png';
+import temumarket from './temumarket.png';
+import temucommunity from './temucommunity.png';
+
+export {
+    temulogoinvert,
+    temuhome,
+    temuinterest,
+    temugoals,
+    temucommunity,
+    temumarket,
+}
