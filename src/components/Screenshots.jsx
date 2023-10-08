6f75f7ba45c76ab96f843aa02bd16bddcb7c9f32
Jason Limas <jasonandrea14@gmail.com>
2023-10-09 04:31:46 +0700

    Hide bottom part of the phone images in SS

--- src/components/Screenshots.jsx
+++ src/components/Screenshots.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from '../style';
+import { temugoals, temuinterest, temumarket, temucommunity } from '../assets';
 
 const Screenshots = () => {
   return (
-    <section id='screenshots' style={{ marginTop: '150px' }} className={`flex flex-col items-center ${styles.paddingY} screenshotsContainer`}>
+    <section id='screenshots' style={{ marginTop: '150px' }} className={`flex flex-col items-center ${styles.paddingY} screenshotsContainer overflow-hidden`}>
       <div className="w-full flex justify-center">
         <h2 className={`text-4xl text-primary font-pointbold`}>
           <div className="max-w-3xl text-center">
@@ -18,8 +19,26 @@ const Screenshots = () => {
           Subscribe
         </button>
       </div>
-      
-      {/* Rest of the Screenshots content will go here */}
+
+      {/* Four-column layout for screenshots */}
+      <div className="flex justify-center items-center mt-12 w-full overflow-hidden image-container" style={{ height: '470px' }}>
+        {/* Column 1 */}
+        <div className="m-2">
+          <img src={temugoals} alt="Temu Goals" className="w-[350px] h-[630px] object-cover shift-up" />
+        </div>
+        {/* Column 2 */}
+        <div className="m-2">
+          <img src={temuinterest} alt="Temu Interest" className="w-[350px] h-[630px] object-cover shift-up" />
+        </div>
+        {/* Column 3 */}
+        <div className="m-2">
+          <img src={temumarket} alt="Temu Market" className="w-[350px] h-[630px] object-cover shift-up" />
+        </div>
+        {/* Column 4 */}
+        <div className="m-2">
+          <img src={temucommunity} alt="Temu Community" className="w-[350px] h-[630px] object-cover shift-up" />
+        </div>
+      </div>
     </section>
   );
 };
